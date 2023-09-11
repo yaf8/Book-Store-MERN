8f3dd127673157a18e76ee1224a9f42e0b927f82
Yafet Abebe Feleke <yafetema15@gmail.com>
2023-09-11 19:14:45 +0300

    initial commit

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongodbUrl = 'mongodb+srv://yaf8:YafetYafet@book-store-mern.xxxh1sj.mongodb.net/BooksCollection?retryWrites=true&w=majority'
---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl hover:py-2 hover:px-2 font-bold underline">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
