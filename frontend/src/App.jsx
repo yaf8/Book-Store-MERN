745a663cbe4e917c09ad250b08e0426f5d9382d2
Yafet Abebe Feleke <yafetema15@gmail.com>
2023-09-14 01:19:30 +0300

    V1.0

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,21 @@
-/* eslint-disable no-unused-vars */
-import React from "react";
-import { Routes, Route } from 'react-router-dom'
-import { Home, CreateBook, DeleteBook, EditBook, ShowBook } from "./pages"
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CreateBook from './pages/CreateBooks';
+import ShowBook from './pages/ShowBook';
+import EditBook from './pages/EditBook';
+import DeleteBook from './pages/DeleteBook';
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/books/create" element={<CreateBook />} />
-      <Route path="/books/details/:id" element={<ShowBook />} />
-      <Route path="/books/edit/:id" element={<EditBook />} />
-      <Route path="/books/delete/:id" element={ <DeleteBook />} />
+      <Route path='/' element={<Home />} />
+      <Route path='/books/create' element={<CreateBook />} />
+      <Route path='/books/details/:id' element={<ShowBook />} />
+      <Route path='/books/edit/:id' element={<EditBook />} />
+      <Route path='/books/delete/:id' element={<DeleteBook />} />
     </Routes>
-  )
-}
+  );
+};
 
 export default App;
--- frontend/src/component/index.js
+++
@@ -1,5 +0,0 @@
-import Spinner from "./Spinner";
-
-export {
-    Spinner
-}
